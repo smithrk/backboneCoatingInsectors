58e673071c091fbb82e63e3428b3d5f0d8388dca
smithrk <ryan.smith@coatinginspectors.com>
2015-03-16 20:31:05 -0700

    minor updates

--- js/views/home/HomeView.js
+++ js/views/home/HomeView.js
@@ -10,15 +10,16 @@ define([
   'views/search_results/ProjectGridView',
   'text!templates/home/homeTemplate.html',
   'text!templates/search_forms/movieSearchFormTemplate.html',
-  'text!templates/home/slideshowTemplate.html'
+  'text!templates/components/slideshowTemplate.html',
+  'text!templates/components/sidebarTemplate.html'
 
-], function($, _, Backbone, ProjectGridView, homeTemplate, movieSearchFormTemplate, slideshow){
+], function($, _, Backbone, ProjectGridView, homeTemplate, movieSearchFormTemplate, slideshow, sidebar){
 
   var HomeView = Backbone.View.extend({
     el: $("#page"),
     render: function(){
         // display home template to the defined element "el"
-        this.$el.html(slideshow + homeTemplate);   
+        this.$el.html(slideshow + homeTemplate + sidebar);   
         
         // display the search form on the home screen
         // demonstrating how you can pass element selector or other values to models to manipulate their
